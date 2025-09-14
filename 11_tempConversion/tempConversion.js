a22f2181bd74b32453255829df5146f570c0b41a
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2025-09-14 14:46:10 +0300

    Completed exercise form 06 to 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  let result = (fahrenheit - 32) * (5 / 9);  
+  result = Number(Number.parseFloat(result).toFixed(1));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  let result = celsius * (9 / 5) + 32; 
+  result = Number(Number.parseFloat(result).toFixed(1));
+  return result;
 };
 
 // Do not edit below this line
