cafee44a0c7f2dd2bcad590a1ec68b999801cb33
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2025-09-16 16:59:01 +0300

    completed calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(numberOne, numberTwo) {
+	return +numberOne + +numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numberOne, numberTwo) {
+	return +numberOne - +numberTwo;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+
+	return array.reduce((total, currentNumber) =>
+    total += currentNumber, 0);
+
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, currentNumber) =>
+    product *= currentNumber, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	// return base ** exponent;
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorialOfNumber = 1;
+  for(let i = number; i > 1; i--){
+    factorialOfNumber *= i;
+  }
+
+  return factorialOfNumber
 };
 
 // Do not edit below this line
