a1f89aad6c95bec512d8bc77bde716c249cf4a28
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2025-09-16 18:15:51 +0300

    completed two exercises(15 and 16)

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    function compare(a, b){
+        let aAge = 0;
+        let bAge = 0;
+        if (a.yearOfDeath === undefined){
+            aAge = 2025 - a.yearOfBirth; 
+        }else{
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        if (b.yearOfDeath === undefined){
+            bAge = 2025 - b.yearOfBirth; 
+        }else{
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
 
+        return bAge - aAge;
+    }
+
+    array.sort(compare);
+
+    return array[0];
 };
 
 // Do not edit below this line
