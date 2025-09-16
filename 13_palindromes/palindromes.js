770f1e00b742329c70b017e08eb4d9873af6cf05
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2025-09-16 17:30:40 +0300

    completed palindromes exercise by learning more about regex

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let alphanumericOnlyString = "";
+    for(let char of string.toLowerCase()){
+        if (/[a-z | 0-9]/.test(char) && char.trim() !== ""){
+            alphanumericOnlyString += char;
+        }
+    }
+    return alphanumericOnlyString === alphanumericOnlyString.split("").reverse().join("");
 };
 
 // Do not edit below this line
