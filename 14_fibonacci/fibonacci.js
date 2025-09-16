87bf8b4e93e8cd7f08a0269d1364366d763e45f5
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2025-09-16 17:46:54 +0300

    completed fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0){
+        return "OOPS";
+    }
 
+    return fib(+number);
+};
+
+function fib(num){
+    if(num < 2){
+        return num;
+    }
+
+    return fib(num - 1) + fib(num - 2)
 };
 
 // Do not edit below this line
